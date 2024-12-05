15a61bcb9ab76894985f033cafd4443ce30947ed
marco <marcoliu666@proton.me>
2024-12-05 10:30:43 +0800

    条件

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ void sort(int arr[], int size) {
     for (int i =0; i < size-1; ++i) {
         for(int j = 0; j < size-1-i; ++j) {
             // marco修改排序算法，为从大到小
-            if(arr[j] > arr[j+1]) {
+            if(arr[j] < arr[j+1]) {
                 tmp = arr[j];
                 arr[j] =arr[j+1];
                 arr[j+1] = tmp;
