2f2a05cbd2b516941e9dd88fb6af76d2ea6c761b
shiteThings <155348194+shiteThings@users.noreply.github.com>
2024-12-05 10:14:14 +0800

    Update main.cpp
    优化冒泡排序算法

--- main.cpp
+++ main.cpp
@@ -4,14 +4,18 @@ using namespace std;
 //shitethis有添加注释
 void sort(int arr[], int size) {
     int tmp = 0;
+    bool flag = false;
     for (int i =0; i < size-1; ++i) {
         for(int j = 0; j < size-1-i; ++j) {
             if(arr[j] > arr[j+1]) {
                 tmp = arr[j];
                 arr[j] =arr[j+1];
                 arr[j+1] = tmp;
+                flag = true;
             }
         }
+        if(!flag)
+            break;
     }   
 }
 int main() {
